b8497808b963e940fc37d6ec4092d48f5c4d50e6
Sergey Muravskiy <0717376@gmail.com>
2024-06-25 22:19:11 +0400

    commit

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
-import os
-import sys
 import marqo
+import os
 from git import Repo
 import shutil
 import re
@@ -8,14 +7,8 @@ from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 import nltk
 
-nltk.download('punkt', quiet=True)
-nltk.download('stopwords', quiet=True)
-
-def get_env_variable(var_name):
-    value = os.getenv(var_name)
-    if value is None:
-        raise EnvironmentError(f"Переменная окружения {var_name} не установлена")
-    return value
+nltk.download('punkt')
+nltk.download('stopwords')
 
 def clone_repo(repo_url, repo_path, username, password):
     if os.path.exists(repo_path):
@@ -31,53 +24,46 @@ def preprocess_text(text):
     return filtered_text
 
 def index_documents(repo_url, repo_path, base_url, username, password):
+    # Клонируем репозиторий
+    clone_repo(repo_url, repo_path, username, password)
+    
+    mq = marqo.Client(url='http://marqo:8882')
+
+    # Удаляем индекс, если он существует
     try:
-        clone_repo(repo_url, repo_path, username, password)
-        
-        mq = marqo.Client(url=get_env_variable('MARQO_URL'))
+        mq.index("my-markdown-index").delete()
+    except:
+        pass
 
-        try:
-            mq.index("my-markdown-index").delete()
-        except:
-            pass
+    # Создаем новый индекс с подходящей моделью
+    settings = {
+        "treatUrlsAndPointersAsImages": False,
+        #"model": "sentence-transformers/stsb-xlm-r-multilingual",
+        "model": "hf/multilingual-e5-large",
+        "normalizeEmbeddings": True,
+    }
+    mq.create_index("my-markdown-index", settings_dict=settings)
 
-        settings = {
-            "treatUrlsAndPointersAsImages": False,
-            "model": "hf/multilingual-e5-large",
-            "normalizeEmbeddings": True,
-        }
-        mq.create_index("my-markdown-index", settings_dict=settings)
-
-        for root, dirs, files in os.walk(repo_path):
-            for file in files:
-                if file.endswith(".md"):
-                    file_path = os.path.join(root, file)
-                    with open(file_path, 'r') as f:
-                        content = f.read()
-                        preprocessed_content = preprocess_text(content)
-                        relative_path = os.path.relpath(file_path, repo_path)
-                        document_url = f"{base_url}/{relative_path.replace(os.path.sep, '/')}"
-                        mq.index("my-markdown-index").add_documents([
-                            {
-                                "Title": file,
-                                "Content": preprocessed_content,
-                                "URL": document_url
-                            }
-                        ], tensor_fields=["Content"])
-        print("Индексация завершена успешно")
-    except Exception as e:
-        print(f"Произошла ошибка при индексации: {str(e)}")
-        sys.exit(1)
+    # Рекурсивно обходим все папки и файлы в клонированном репозитории
+    for root, dirs, files in os.walk(repo_path):
+        for file in files:
+            if file.endswith(".md"):
+                file_path = os.path.join(root, file)
+                with open(file_path, 'r') as f:
+                    content = f.read()
+                    preprocessed_content = preprocess_text(content)
+                    relative_path = os.path.relpath(file_path, repo_path)
+                    document_url = f"{base_url}/{relative_path.replace(os.path.sep, '/')}"
+                    mq.index("my-markdown-index").add_documents([
+                        {
+                            "Title": file,
+                            "Content": preprocessed_content,
+                            "URL": document_url
+                        }
+                    ], tensor_fields=["Content"])
 
 if __name__ == '__main__':
-    try:
-        repo_url = get_env_variable('REPO_URL')
-        repo_path = get_env_variable('REPO_PATH')
-        base_url = get_env_variable('BASE_URL')
-        username = get_env_variable('GIT_USERNAME')
-        password = get_env_variable('GIT_PASSWORD')
-
-        index_documents(repo_url, repo_path, base_url, username, password)
-    except EnvironmentError as e:
-        print(f"Ошибка: {str(e)}")
-        sys.exit(1)
+    repo_url = 'https://0717376:GCXPO89msv@gitlab.muravskiy.com/0717376/requirements.git'
+    repo_path = os.path.expanduser("~/Desktop/repository")
+    base_url = 'https://gitlab.muravskiy.com/0717376/requirements/blob/main'  # URL к репозиторию на GitLab
+    index_documents(repo_url, repo_path, base_url, '0717376', 'GCXPO89msv')
